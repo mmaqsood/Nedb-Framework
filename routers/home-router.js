dbd6cbce84b050fae261445eafc9bddc2b39c9ee
MMAQSOOD <MMAQSOOD99@GMAIL.COM>
2015-02-03 23:24:41 -0800

    Changed Routing & added nice front page

--- routers/home-router.js
+++ routers/home-router.js
@@ -1,7 +1,7 @@
 module.exports = {
 	setup: function(app){
 		app.get('/', function (req, res) {
-		    res.render('index', {
+		    res.render('master', {
 		        content: "Hello"
 		    });
 		})
